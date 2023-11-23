44d0d034fa661b306eb6ebe1485ce23535b76add
VelocyraptorZ <khokhonicho@gmail.com>
2023-11-23 09:43:20 +0700

    Version 0.1

--- Modules/Website/Resources/views/layouts/master.blade.php
+++ Modules/Website/Resources/views/layouts/master.blade.php
@@ -6,8 +6,8 @@
 
     <title>Laravel</title>
 
-    <link rel="icon" href="{{ url('css/favicon.jpg') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('template/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('template/css/style.css') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,5 +20,7 @@
 </head>
 <body class="antialiased">
     @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
