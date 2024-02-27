479b07291786e7c005e75f796e6506684440b3eb
VelocyraptorZ <khokhonicho@gmail.com>
2024-02-27 18:56:34 +0700

    Version 0.7 Update Language Switcher

--- Modules/Website/Resources/views/index.blade.php
+++ Modules/Website/Resources/views/index.blade.php
@@ -5,21 +5,25 @@
         <nav class="navbar nav-head">
             <div class="container-fluid">
                 <a class="navbar-brand">PT Jinyoung Sukses Indonesia</a>
-                <form class="d-flex" role="search">
+                <form class="d-flex mb-0" role="search">
                     <button class="btn btn-primary me-2">
                         <a class="text-white text-decoration-none" href="{{ route('login') }}">Login</a>
                     </button>
                     <div class="dropdown me-2">
                         <button class="btn btn-secondary" data-bs-toggle="dropdown">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-translate" viewBox="0 0 16 16">
-                                <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286zm1.634-.736L5.5 3.956h-.049l-.679 2.022z"/>
-                                <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zm7.138 9.995q.289.451.63.846c-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6 6 0 0 1-.415-.492 2 2 0 0 1-.94.31"/>
-                            </svg>
+                            <img src="https://cdn.parcellab.com/img/flags/id.png">ID
                         </button>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">lang</a></li>
-                            <li><a class="dropdown-item" href="#">lang</a></li>
-                            <li><a class="dropdown-item" href="#">lang</a></li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <img src="https://cdn.parcellab.com/img/flags/us.png"> EN
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <img src="https://cdn.parcellab.com/img/flags/id.png"> IN
+                                </a>
+                            </li>
                         </ul>
                     </div>
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
