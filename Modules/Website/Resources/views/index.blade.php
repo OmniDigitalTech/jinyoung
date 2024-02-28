58eb90b6027a7a32a0d4b53f56fc38cf8ce956e0
VelocyraptorZ <khokhonicho@gmail.com>
2024-02-28 11:12:23 +0700

    Comment Search Form

--- Modules/Website/Resources/views/index.blade.php
+++ Modules/Website/Resources/views/index.blade.php
@@ -9,9 +9,9 @@
                     <button class="btn btn-primary me-2">
                         <a class="text-white text-decoration-none" href="{{ route('login') }}">Login</a>
                     </button>
-                    <div class="dropdown me-2">
+                    <div class="dropdown">
                         <button class="btn btn-secondary" data-bs-toggle="dropdown">
-                            <img src="https://cdn.parcellab.com/img/flags/id.png">ID
+                            <img src="https://cdn.parcellab.com/img/flags/id.png" width="25" height="22">
                         </button>
                         <ul class="dropdown-menu">
                             <li>
@@ -26,12 +26,12 @@
                             </li>
                         </ul>
                     </div>
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-primary" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                        </svg>
-                    </button>
+{{--                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">--}}
+{{--                    <button class="btn btn-outline-primary" type="submit">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">--}}
+{{--                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>--}}
+{{--                        </svg>--}}
+{{--                    </button>--}}
                 </form>
             </div>
         </nav>
