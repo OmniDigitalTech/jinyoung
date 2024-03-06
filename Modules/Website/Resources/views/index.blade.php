8d3c4672e2407a3a5b7276ee759c5531f8663757
VelocyraptorZ <khokhonicho@gmail.com>
2024-03-06 11:14:51 +0700

    update step modal

--- Modules/Website/Resources/views/index.blade.php
+++ Modules/Website/Resources/views/index.blade.php
@@ -282,10 +282,8 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="product-grid">
-                            <div class="product-image">
-                            </div>
-                            <div class="product-content product-image">
+                        <div class="step-modal-grid">
+                            <div class="step-modal-content step-modal-image">
                                 <img id="image-process" src="">
                             </div>
                         </div>
