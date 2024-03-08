ff11b870a0ec8ca9cfa4d55a9b24f0d234b03378
VelocyraptorZ <khokhonicho@gmail.com>
2024-03-08 18:24:24 +0700

    add slick slider responsive

--- Modules/Website/Resources/views/layouts/master.blade.php
+++ Modules/Website/Resources/views/layouts/master.blade.php
@@ -31,6 +31,30 @@
                 speed: 300,
                 slidesToShow: 4,
                 slidesToScroll: 4,
+                responsive: [
+                            {
+                                breakpoint: 1024,
+                                settings: {
+                                    slidesToShow: 4,
+                                    slidesToScroll: 4,
+                                }
+                            },
+                            {
+                                breakpoint: 600,
+                                settings: {
+                                    slidesToShow: 3,
+                                    slidesToScroll: 3
+                                }
+                            },
+                            {
+                                breakpoint: 480,
+                                settings: {
+                                    row : 3,
+                                    slidesToShow: 2,
+                                    slidesToScroll: 2
+                                }
+                            }
+                        ]
             });
         });
     </script>
