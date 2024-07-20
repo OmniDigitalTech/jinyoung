c9abe806952b47c5c8e1be4560ddf1307e0b69e9
Rey Asli <reyvaldowildan7298@gmail.com>
2024-07-20 15:25:30 +0700

    download

--- Modules/Management/Http/Controllers/BackupController.php
+++ Modules/Management/Http/Controllers/BackupController.php
@@ -16,7 +16,15 @@ class BackupController extends Controller
     }
 
     public function download(Picture $picture){
-        $filePath = storage_path('app/public/images/' . $picture->file_name);
+//        $filePath = storage_path('app/public/images/' . $picture->file_name);
+
+        $fileName = basename($picture->url);
+
+        $tempImage = tempnam(sys_get_temp_dir(), $fileName);
+        copy($picture->url, $tempImage);
+
+        return response()->download($tempImage, $fileName)->deleteFileAfterSend(true);
+
         return response()->download($filePath);
     }
 }
